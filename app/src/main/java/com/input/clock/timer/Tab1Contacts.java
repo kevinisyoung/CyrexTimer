5059e66b636ca3a249a9dfa73344efd74902fc6b
kevinisyoung <kevindyoung76@gmail.com>
2016-12-29 21:23:20 -0800

    Initial commit

---
+++ app/src/main/java/com/input/clock/timer/Tab1Contacts.java
@@ -0,0 +1,18 @@
+package com.input.clock.timer;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Tab1Contacts extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1contacts, container, false);
+        return rootView;
+    }
+}
